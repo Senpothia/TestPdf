4eaf76405befdda10211abe756d81d37abc69aff
Michel Lopez <michel@cdvi.com>
2020-09-01 16:24:10 +0200

    init

---
+++ src/main/java/PDFGenerator.java
@@ -0,0 +1,32 @@
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class PDFGenerator {
+	
+	public static final String DEST = "C:\\Users\\michel\\hello.pdf";
+
+	public static void main(String[] args) {
+
+		try {
+			Document doc = new Document();
+			PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(DEST));
+			// setting font family, color
+			Font font = new Font(Font.HELVETICA, 16, Font.BOLDITALIC, Color.RED);
+			doc.open();
+			Paragraph para = new Paragraph("Hello! This PDF is created using openPDF", font);
+			doc.add(para);
+			doc.close();
+			writer.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
